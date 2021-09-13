f3ca156956f6dda09acc0b564e6e382ca6dbbb5a
Mamata890 <mamata.moger@gmail.com>
2021-09-12 23:48:14 -0400

    new

---
+++ build/precache-manifest.6824ea608ec5dcfe28bd63a066c63c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245f7d45c76cc2885ca805f35486ee2f",
+    "url": "/Mamata890/new_monster/index.html"
+  },
+  {
+    "revision": "303d854b4e2405c4d5ce",
+    "url": "/Mamata890/new_monster/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "660f4d062e4822a1848f",
+    "url": "/Mamata890/new_monster/static/js/2.62c2cd20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Mamata890/new_monster/static/js/2.62c2cd20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "303d854b4e2405c4d5ce",
+    "url": "/Mamata890/new_monster/static/js/main.eba50612.chunk.js"
+  },
+  {
+    "revision": "101403312aa680d71c59",
+    "url": "/Mamata890/new_monster/static/js/runtime-main.dac22965.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mamata890/new_monster/static/media/logo.5d5d9eef.svg"
+  }
+]);
